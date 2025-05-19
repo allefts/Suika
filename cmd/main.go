330ab6145c65de2114372034fea6c6c5b1803653
allefts <alleftsoares@gmail.com>
2025-05-19 15:31:55 -0500

    working collision bounds at least

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"image"
+	"image/color"
 	"math/rand/v2"
 
 	"github.com/allefts/suika/models"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 const (
@@ -36,9 +37,9 @@ func (g *Game) Update() error {
 
 	if len(g.PlayedFruits) != 0 {
 		lastDroppedFruit := g.PlayedFruits[len(g.PlayedFruits)-1]
-		for _, collider := range g.Colliders {
-			if collider.Overlaps(image.Rect(lastDroppedFruit.X, lastDroppedFruit.Y, lastDroppedFruit.X+16, lastDroppedFruit.Y+32)) {
-				fmt.Println("Fruit Collision")
+		for i := 0; i < len(g.PlayedFruits)-1; i++ {
+			if g.Colliders[i].Overlaps(image.Rect(lastDroppedFruit.X-16, lastDroppedFruit.Y, lastDroppedFruit.X+16, lastDroppedFruit.Y+32)) {
+				// fmt.Println(lastDroppedFruit.Name, g.PlayedFruits[i].Name)
 			}
 		}
 	}
@@ -49,6 +50,10 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	drawCurrFruit(screen, g.Queue[0].Img, g.MouseX)       //Draw Top Fruit
 	drawPlayedFruits(screen, g.PlayedFruits, g.Colliders) //Draw Played Fruits
+
+	for _, collider := range g.Colliders {
+		vector.StrokeRect(screen, float32(collider.Min.X), float32(collider.Min.Y), float32(collider.Dx()), float32(collider.Dy()), 1.0, color.RGBA{255, 0, 0, 255}, true)
+	}
 }
 
 func (g *Game) dropCurrFruit() {
@@ -57,9 +62,9 @@ func (g *Game) dropCurrFruit() {
 		g.Queue[0].X = g.MouseX
 		g.Queue[0].Y = 5
 
-		g.PlayedFruits = append(g.PlayedFruits, g.Queue[0])                                     //Add to played fruits
-		newCollider := image.Rect(g.Queue[0].X, g.Queue[0].Y, g.Queue[0].X+32, g.Queue[0].Y+32) //New collider
-		g.Colliders = append(g.Colliders, &newCollider)                                         //Append to colliders
+		g.PlayedFruits = append(g.PlayedFruits, g.Queue[0])                                        //Add to played fruits
+		newCollider := image.Rect(g.Queue[0].X-16, g.Queue[0].Y, g.Queue[0].X+16, g.Queue[0].Y+32) //New collider
+		g.Colliders = append(g.Colliders, &newCollider)                                            //Append to colliders
 
 		g.Queue = g.Queue[1:]                                       //Remove from Queue
 		g.Queue = append(g.Queue, models.CreateFruit(rand.IntN(4))) //Append new fruit to queue
@@ -68,7 +73,7 @@ func (g *Game) dropCurrFruit() {
 }
 
 func drawPlayedFruits(screen *ebiten.Image, playedFruits []*models.Fruit, colliders []*image.Rectangle) {
-	for _, fruit := range playedFruits {
+	for idx, fruit := range playedFruits {
 		if fruit.Y < (screenHeight - frameHeight) {
 			fruit.Y += 1
 		}
@@ -77,6 +82,9 @@ func drawPlayedFruits(screen *ebiten.Image, playedFruits []*models.Fruit, collid
 		op.GeoM.Translate(float64(-frameWidth/2), 0)
 		op.GeoM.Translate(float64(fruit.X), float64(fruit.Y))
 		screen.DrawImage(fruit.Img, op)
+
+		colliders[idx].Min = image.Point{X: fruit.X - 16, Y: fruit.Y}
+		colliders[idx].Max = image.Point{X: fruit.X + 16, Y: fruit.Y + 32}
 	}
 }
 
